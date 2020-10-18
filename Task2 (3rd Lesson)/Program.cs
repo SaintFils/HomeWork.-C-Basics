bd272dc23ed151e9ba7ab0d0ad731fbec46c6f3d
Oleg <gurmanoleg@yandex.ru>
2020-10-18 14:02:21 +0300

    Добавлено второе задание

--- Task2 (3rd Lesson)/Program.cs
+++ Task2 (3rd Lesson)/Program.cs
@@ -8,8 +8,23 @@ namespace Task2__3rd_Lesson_
 {
     class Program
     {
+        private static int NumOfDigits()
+        {
+            int s = 0;
+            string a = Console.ReadLine();
+            while (int.TryParse(a, out int res) && res != 0)
+            {
+                if (res > 0 && res % 2 != 0) s += res;
+                a = Console.ReadLine();
+            }
+
+            return s;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите числа: ");
+            Console.WriteLine($"Сумма всех нечетных положительных чисел равна {NumOfDigits()}");
+            Console.ReadLine();
         }
     }
 }
