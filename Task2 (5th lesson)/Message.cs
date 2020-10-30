14cee23389684800450ddad9220ac66493814e10
Oleg <gurmanoleg@yandex.ru>
2020-10-30 17:39:09 +0300

    HW 5.2

---
+++ Task2 (5th lesson)/Message.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2__5th_lesson_
+{
+    static class Message
+    {
+        /// <summary>
+        /// Метод для создания массива слов с длинной не более указанной
+        /// </summary>
+        /// <param name="message">сообщение из которого нужно получить слова</param>
+        /// <param name="lengthWord">максимальная длина слова</param>
+        /// <param name="separator">разделитель</param>
+        /// <returns>Возвращается массив слов</returns>
+        static public string[] PrintN (string message, int lengthWord, params char[] separator)
+        {
+            string[] words = message.Split(separator);
+            string[] result = new string[0];
+            int counter = 0;
+
+            foreach (var word in words) 
+            {
+                if(word.Length <= lengthWord)
+                {
+                    Array.Resize(ref result, result.Length + 1);
+                    result[counter++] = word;
+                }
+
+            }
+            return result;
+        }
+        /// <summary>
+        /// Метод удаляет слова которые оканчиваются на конкретный символ
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <param name="sym">символ</param>
+        /// <param name="separator">разделитель</param>
+        /// <returns>возвращает сообщение со словами, которые не содержат указанный символ</returns>
+        static public string LastChar(string message, char sym, params char[] separator)
+        {
+            string[] words = message.Split(separator);
+            string result = String.Empty;
+            foreach (var word in words)
+            {
+                if(word[word.Length - 1] != sym) //не забывать, что индексация идет с нуля и для обращения к последнему символу нужно вычесть 1!!!
+                {
+                    result += $"{word} ";
+                } 
+            }
+            return result;
+        }
+        /// <summary>
+        /// Метод находит самое длинное слово в сообщении
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <param name="separator">разделитель</param>
+        /// <returns>возвращает самое длинное слово</returns>
+        static public string Longest(string message, params char[] separator)
+        {
+            string[] words = message.Split(separator);
+            int temp = words[0].Length;
+            string result = words[0];
+            foreach (var word in words)
+            {
+                if(word.Length > temp)
+                {
+                    temp = word.Length;
+                    result = word;
+                }
+            }
+            return result;
+
+        }
+
+        static public StringBuilder LongestMessage(string message, params char[] separator)
+        {
+            string[] words = message.Split(separator);
+            int temp = words[0].Length;
+            //string result = words[0];
+            StringBuilder result = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                if (word.Length > temp)
+                {
+                    temp = word.Length;                   
+                }
+            }
+            foreach (var word in words)
+            {
+                if (word.Length == temp)
+                {
+                    //result += word + " ";
+                    result.Append($"{word} ");
+                }
+            }
+            return result;
+        }
+
+
+
+    }
+}
--- Task2 (5th lesson)/Program.cs
+++ Task2 (5th lesson)/Program.cs
@@ -10,6 +10,50 @@ namespace Task2__5th_lesson_
     {
         static void Main(string[] args)
         {
+            // Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+            // а) Вывести только те слова сообщения,  которые содержат не более n букв.
+            // б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+            // в) Найти самое длинное слово сообщения.
+            // г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+            // д) ***Создать метод, который производит частотный анализ текста.
+            // В качестве параметра в него передается массив слов и текст,
+            //в качестве результата метод возвращает сколько раз каждое из слов массива входит в этот текст.
+            //Здесь требуется использовать класс Dictionary.
+            #region a)
+            Console.WriteLine("Введите сообщение:");
+            string tempA = Console.ReadLine();
+            Console.WriteLine("Введите максимальное число символов:");
+            int numA = Convert.ToInt32(Console.ReadLine());
+            string[] resultA = Message.PrintN(tempA, numA, ' ');
+            foreach (var item in resultA)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            #endregion
+
+            #region b)
+            Console.WriteLine("Введите сообщение:");
+            string tempB = Console.ReadLine();
+            Console.WriteLine("Введите символ:");
+            char symB = Convert.ToChar(Console.ReadLine());
+            string resultB = Message.LastChar(tempB, symB, ' ');
+            Console.WriteLine($"{resultB}"); 
+            #endregion
+
+            #region с)
+            Console.WriteLine("Введите сообщение:");
+            string tempC = Console.ReadLine();
+            Console.WriteLine($"{Message.Longest(tempC, ' ')}");
+            Console.ReadLine();
+            #endregion
+
+            #region d)
+            string tempD = "p pp ppp pppp ppppp o oo ooo oooo ooooo i ii iiii iii iiiii";
+            Console.WriteLine($"{Message.LongestMessage(tempD, ' ')}");
+            #endregion
+
+
         }
     }
 }
